9be3b7440093e20ca8f91a8fc3fb328637c4ac1d
Elecravero <91529865+Elecravero@users.noreply.github.com>
2021-11-09 19:04:59 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,23 @@
 using namespace std;
 
 int main() {
+   int primo=1, secondo=1, terzo=0;
+     int n=0;
+     cin>>n;
+
+     if(n<2){
+         cout<<"errore"<<endl;
+     }else {
+         cout << primo << endl;
+         cout << secondo<< endl;
+
+         for (int i = 1; i < n; i++) {
+             terzo = primo + secondo;
+             primo = secondo;
+             secondo = terzo;
+             cout <<terzo<<endl;
+         }
+
+     }
    return 0;
 }
